f0882da7561c417be5b69d83955825279181f484
David Torres <jdtcto@gmail.com>
2023-05-12 17:43:30 -0500

    NWEB-323: add split override fields

--- schemas/objects/page/components/molecules/dzSplit.ts
+++ schemas/objects/page/components/molecules/dzSplit.ts
@@ -77,6 +77,30 @@ export default defineType({
       initialValue: false
     }),
     defineField({
+      name: 'title',
+      type: 'string',
+      title: 'Component title',
+      group: 'content',
+    }),
+    defineField({
+      name: 'titleOverride',
+      type: 'string',
+      title: 'Component title',
+      group: 'overrides',
+    }),
+    defineField({
+      name: 'ctaOverride',
+      type: 'string',
+      title: 'CTA title',
+      group: 'overrides',
+    }),
+    defineField({
+      name: 'subtitleOverride',
+      type: 'string',
+      title: 'Component subtitle',
+      group: 'overrides',
+    }),
+    defineField({
       name: 'imageOverride',
       type: 'image',
       title: 'Image',
