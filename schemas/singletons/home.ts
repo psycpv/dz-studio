5d309adce4ba6e3fd617c45abe90bc06d9bf8a21
David Torres <jdtcto@gmail.com>
2023-05-12 20:25:31 -0500

    NWEB-323: add locations as a reference, fix required rule

--- schemas/singletons/home.ts
+++ schemas/singletons/home.ts
@@ -36,7 +36,7 @@ export default defineType({
       title: 'Title',
       type: 'string',
       group: 'content',
-      validation: (Rule) => Rule.required(),
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'header',
@@ -79,7 +79,7 @@ export default defineType({
       title: 'Locations',
       group: 'content',
       type: 'array',
-      of: [{type: 'location'}],
+      of: [{type: 'reference', name: 'location', to: {type: 'location'}}],
     }),
   ],
 })
