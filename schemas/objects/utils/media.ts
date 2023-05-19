f80916d186f0843ee4b435c445f720335e04c85a
Fabio Tovar <fabiostovarr@gmail.com>
2023-05-19 15:48:29 -0500

    Update media file

--- schemas/objects/utils/media.ts
+++ schemas/objects/utils/media.ts
@@ -1,9 +1,10 @@
-//TODO determine if we need the design system import {MEDIA_TYPES} from '@zwirner/design-system'
-import {defineArrayMember, defineField} from 'sanity'
+import {defineField} from 'sanity'
+
 export const MEDIA_TYPES = {
   IMAGE: 'image',
   VIDEO: 'video',
-};
+}
+
 export default defineField({
   name: 'media',
   title: 'Media',
@@ -13,7 +14,6 @@ export default defineField({
       name: 'type',
       title: 'Media Type',
       type: 'string',
-      description: 'Choose between picture or video',
       options: {
         list: [
           {title: 'Image', value: MEDIA_TYPES.IMAGE},
@@ -22,47 +22,43 @@ export default defineField({
       },
     }),
     defineField({
-      title: 'Pictures',
-      name: 'pictures',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          type: 'image',
-          options: {
-            hotspot: true,
-          },
-          fields: [
-            {
-              name: 'alt',
-              type: 'string',
-              title: 'Alternative text',
-            },
-          ],
-        }),
-      ],
-      options: {
-        layout: 'grid',
-      },
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      fields: [defineField({name: 'alt', title: 'Alternative text', type: 'string'})],
       hidden: ({parent}) => {
-        return parent?.link || !parent?.type || parent?.type === MEDIA_TYPES.VIDEO
+        return parent?.type === MEDIA_TYPES.VIDEO || !parent?.type
       },
     }),
     defineField({
-      name: 'linkImage',
-      title: 'Image Link CTA',
-      type: 'link',
-      description: 'Link for the image CTA',
-      hidden: ({parent}) => {
-        return parent?.type === MEDIA_TYPES.VIDEO || !parent?.type || parent?.pictures
-      },
-    }),
-    defineField({
-      name: 'linkVideo',
-      title: 'Video Link',
-      type: 'link',
-      description: 'Link for the source of the video',
+      name: 'provider',
+      title: 'Provider',
+      type: 'string',
+      options: {list: ['vimeo', 'youtube', 'custom']},
       hidden: ({parent}) => {
         return parent?.type === MEDIA_TYPES.IMAGE || !parent?.type
+      },
+    }),
+    defineField({
+      name: 'video',
+      title: 'Video',
+      type: 'file',
+      options: {accept: 'video/mp4,video/x-m4v,video/*'},
+      hidden: ({parent}) => {
+        return parent?.type === MEDIA_TYPES.IMAGE || !parent?.type || parent?.provider !== 'custom'
+      },
+    }),
+    defineField({
+      name: 'externalVideo',
+      title: 'Video URL',
+      type: 'url',
+      options: {accept: 'video/mp4,video/x-m4v,video/*'},
+      hidden: ({parent}) => {
+        return (
+          parent?.type === MEDIA_TYPES.IMAGE ||
+          !parent?.type ||
+          !['vimeo', 'youtube'].includes(parent?.provider)
+        )
       },
     }),
   ],
