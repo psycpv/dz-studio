67327956c557c01329af878101d0107b2e3e0996
Ryan Sturt <rsturt@davidzwirner.com>
2023-06-21 19:12:30 -0400

    removes strong from tagline

--- schemas/documents/book.ts
+++ schemas/documents/book.ts
@@ -34,7 +34,6 @@ export default defineType({
             lists: [],
             marks: {
               decorators: [
-                {title: 'Strong', value: 'strong'},
                 {title: 'Emphasis', value: 'em'},
                 {title: 'Underline', value: 'underline'},
               ],
