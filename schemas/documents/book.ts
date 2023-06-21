2e818d953ceb8f76bd6dde273cfbeddff1cfa591
Ryan Sturt <rsturt@davidzwirner.com>
2023-06-21 18:02:58 -0400

    adds portable text fields and styles

--- schemas/documents/book.ts
+++ schemas/documents/book.ts
@@ -26,7 +26,21 @@ export default defineType({
       title: 'Tagline',
       type: 'array',
       of: [
-        {type: 'block'},
+        {
+            type: 'block',
+            styles: [
+              {title: 'Normal', value: 'normal'},
+            ],
+            lists: [],
+            marks: {
+              decorators: [
+                {title: 'Strong', value: 'strong'},
+                {title: 'Emphasis', value: 'em'},
+                {title: 'Underline', value: 'underline'},
+              ],
+            },
+
+          },
       ],
     }),
     defineField({
@@ -58,7 +72,21 @@ export default defineType({
       title: 'Description',
       type: 'array',
       of: [
-        {type: 'block'},
+        {
+            type: 'block',
+            styles: [
+              {title: 'Normal', value: 'normal'},
+            ],
+            marks: {
+              decorators: [
+                {title: 'Strong', value: 'strong'},
+                {title: 'Emphasis', value: 'em'},
+                {title: 'Underline', value: 'underline'},
+              ],
+            },
+
+          },
+          {type: 'image'},
       ],
     }),
     defineField({
