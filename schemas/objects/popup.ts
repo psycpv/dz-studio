3677ed64bea04f2c1b9ecb91de1cd932a4b0d5dc
Sergei <50995397+MadaShindeInai@users.noreply.github.com>
2023-12-07 19:40:40 +0100

    Update popup.ts (#320)

--- schemas/objects/popup.ts
+++ schemas/objects/popup.ts
@@ -202,7 +202,7 @@ export default defineType({
             rule.custom(async (value, context) => {
               const {type: valueType, image} = (value ?? {}) as MediaValueType
               const {type} = context.parent as {type: PopUpTypes}
-              if (!valueType) {
+              if (!valueType && type !== PopUpTypes['Inquire']) {
                 return 'Media Type should be defined. If an image is not needed, choose "Unset"'
               }
               if (type === PopUpTypes['Custom Promo'] && valueType !== Media.MediaTypes.IMAGE) {
